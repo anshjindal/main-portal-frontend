ca664f598a49eb7e276b9cb34d04acdf37213b08
Ansh Jindal <jindalansh498@gmail.com>
2024-09-13 08:01:57 -0400

    Fixed bug in Navbar

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -22,12 +22,12 @@ function Navbar({ onClick, onMenuItemClick }) {
 
   return (
     <>
-      <div className="flex justify-center pl-[1vw] pr-[1vw] z-1000 ">
+      <div className="flex justify-center pl-[1vw] pr-[1vw] navbar">
         <div className="flex items-center justify-center h-[8vw] border-b-[0.001vw] text-[#2B00AC] border-[#B8B8B8] max-[450px]:w-[80%] max-[450px]:border-none max-[450px]:pt-[13vw] max-[450px]:pb-[9vw]">
           <div className="flex items-center gap-x-[10vw] w-[95%]">
             <Link
               to="frontend-react-wouessi-website-v2/"
-              className="w-[20%] max-[450px]:w-[50%]"
+              className="w-[20%] max-[450px]:w-[50%] z-10"
             >
               <img src={logo} className="cursor-pointer"></img>
             </Link>
@@ -68,9 +68,9 @@ function Navbar({ onClick, onMenuItemClick }) {
         <p>{language}</p>
       </div>
       {languageDropdown && (
-        <div className="absolute top-full left-0 mt-[0.5rem] w-max bg-white border border-gray-300 shadow-lg rounded-md z-10">
+        <div className="absolute left-0 w-max bg-white border border-gray-300 shadow-lg rounded-md">
           <p
-            className="px-4 py-2 hover:bg-[#FF9900] hover:text-white cursor-pointer transition-colors duration-300"
+            className="px-4 py-2 hover:bg-[#FF9900] hover:text-white cursor-pointer transition-colors duration-300" 
             onClick={() => setLanguage("EN")}
           >
             &gt; EN
@@ -87,7 +87,7 @@ function Navbar({ onClick, onMenuItemClick }) {
             </ul>
           </div>
           <Link to="frontend-react-wouessi-website-v2/ContactUs">
-            <div className="max-[450px]:hidden w-[10vw] h-[3.5vw] rounded-full border-[0.1vw] border-[#2B00AC] hover:bg-[#0033A0] hover:text-[#FFFFFF] text-[#2B00AC] flex items-center justify-center transition-all duration-300 ease-in-out relative shadow-inner hover:shadow-inner-custom ">
+            <div className="max-[450px]:hidden w-[10vw] h-[3.5vw] rounded-full border-[0.1vw] z-10 border-[#2B00AC] hover:bg-[#0033A0] hover:text-[#FFFFFF] text-[#2B00AC] flex items-center justify-center transition-all duration-300 ease-in-out relative shadow-inner hover:shadow-inner-custom ">
               Let's Talk
               <CgArrowLongUp className="ml-[1vw] text-[1.5vw] rotate-[90deg]" />
               <div className="absolute inset-0 rounded-full bg-[#0033A0] opacity-0 hover:opacity-20 transition-opacity duration-300 ease-in-out"></div>
